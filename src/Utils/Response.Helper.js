f4a680225713e0bad577bedc91cadb7ee89aab25
everforgeTest <everforgeapiautomation@gmail.com>
2026-04-29 11:36:28 +0530

    Initial commit from Everpower Studio

---
+++ src/Utils/Response.Helper.js
@@ -0,0 +1,18 @@
+module.exports = {
+  success: function(data) {
+    return { success: data };
+  },
+  error: function(code, message) {
+    return { error: { code: code || 500, message: message || "Error" } };
+  }
+};
+module.exports = {
+  success: function(data) {
+    return { success: data };
+  },
+  error: function(code, message) {
+    return { error: { code: code || 500, message: message || "Error" } };
+  }
+}; 
+//hjkhjkhk
+
